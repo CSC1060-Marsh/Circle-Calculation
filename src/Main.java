8bbf15c1083170ed675028835b6b2a9e7aca2803
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-09-15 17:56:12 -0600

    Simplified To First Message

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("What is the radius of your circle?");
-
-        String radius = sc.nextLine();
-
-        }
-        public static double area; {
-            
         }
     }
